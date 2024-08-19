59a70f6bbb4bc8f286b4dbd0cea2bd741958756d
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-19 23:21:14 +0100

    calculate start and end index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 """ Helper function module
 """
+
+
+def index_range(page, page_size):
+    """ Calculate index range """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
