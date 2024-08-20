33d6834b0c228aa75a8fae342bb7b499c3a6d535
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-20 16:03:20 +0100

    LIFO with bugs

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 """LIFO caching Module
 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO caching class """
+
+    def __init__(self):
+        """ Initialize LIFO class """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Put data in cache """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data.keys()) == self.MAX_ITEMS:
+                if key in self.cache_data.keys():
+                    self.cache_data[key] = item
+                    # print("Discard: {}".format(key))
+                else:
+                    last_key = sorted(self.cache_data.keys())[-1]
+                    del self.cache_data[last_key]
+                    print("Discard: {}".format(last_key))
+                    self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get data from cache """
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
+
+        return None
