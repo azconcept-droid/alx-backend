c0377665af08d6a0dbd3f2efcbdc2b6e2b392685
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-20 12:51:46 +0100

    put and get data from cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 """Basic dictionary Module
 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic caching system class """
+    def put(self, key, item):
+        """ Put data in cache """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get data from cache """
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
