7ec589d54cb9837e95f4707737b45bf06fcf55dd
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-06 13:17:05 +0100

    add transelation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Config babel using get_locale function
+Parametrize templates module
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _, lazy_gettext as _l
 
 
 class Config:
@@ -28,4 +28,6 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """Index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html',
+                           home_title=_l('Welcome to Holberton'),
+                           home_header=_l('Hello world'))
